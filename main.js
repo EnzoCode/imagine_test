c4cc832f5c57ad374acd901e3f59c12891f4acaf
Your Name <enzolopespower@gmail.com>
2018-06-13 22:21:32 -0300

    sla

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
 $(function(){
+    let content = $('#content');
+    $('#content').addClass('container');
 
-    //vanilla js
-    document.getElementById('texto1').innerText = 'Texto com Javascript';
-
-    //jQuery id  
-    $('#texto2').text('Texto com jQuery');
-    
-    //jQuery class 
-    $('.texto3').text('Texto com jQuery3');
-
-    //Html class tag
-    $('#html').html('<h1>Cannon Rush</h1>')
-    //editando/adicionando parte do texto
-    $('#html').append('<a>Clique aqui</a>');
-    //editar atributos
-    $('#html a').attr('href', 'http://github.com')
-    //adicionando classes
-    $('#html h1').addClass('red');
-    $('#html a').addClass('red');
-    // Removendo uma classe
-    $('#html a').removeClass('red');
-
-    $('#texto2').css({color: 'blue', fontSize: '30px'});
-    
+    content.append('<div></div>').addClass('jumbotron');
+    $('<h1></h1>').appendTo('#content').addClass('display-4').text('hello word')
 });
